9bfc8d55f7dd931aaeb3dd45b9c9b561db2d85ce
Dan5433 <grishkov.daniil@gmail.com>
2024-10-04 19:41:36 -0400

    Added going up slabs

--- Assets/Scripts/Tiles/SlabTile.cs
+++ Assets/Scripts/Tiles/SlabTile.cs
@@ -7,6 +7,25 @@ public class SlabTile : MonoBehaviour
         collision.transform.parent.GetComponentInChildren<SpriteRenderer>().sortingOrder = 1;
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        var yRelation = collision.transform.parent.position.y - transform.position.y - 0.4f;   
+        if(yRelation < 0 && yRelation > -0.38)
+        {
+            collision.transform.parent.position = 
+                new(collision.transform.parent.position.x,
+                collision.transform.parent.position.y + 0.5f, 
+                collision.transform.parent.position.z);
+        }
+        if (yRelation > 0 && yRelation < 0.18)
+        {
+            collision.transform.parent.position =
+                new(collision.transform.parent.position.x,
+                collision.transform.parent.position.y - 0.5f,
+                collision.transform.parent.position.z);
+        }
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         collision.transform.parent.GetComponentInChildren<SpriteRenderer>().sortingOrder = 0;
